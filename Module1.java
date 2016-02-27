122af5809a3e08aa526a37eff0ab000b8b3d53ca
ded2055 <ded2055@yandex.ru>
2016-02-27 18:13:30 +0200

    correction

--- Module1.java
+++ Module1.java
@@ -1,5 +1,5 @@
 public class Module1{
 	public static void main(String[] args){
-		System.out.print("\"Hi, I'm your first Java program! Stay tuned!\"");
+		System.out.println("Hi, I'm your first Java program! Stay tuned!);
 	}
 }
